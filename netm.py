969c481b89ceb7090cbb913c6b63836b520412dd
Daniel Graham <dnlgrhm@gmail.com>
2025-03-14 07:25:49 -0400

    Cleanup and rename

--- netm.py
+++ netm.py
@@ -1,28 +1,38 @@
+#!/usr/bin/env python3
 from etm.controller import Controller
-from etm.view_rich import FourWeekView
-from prompt_toolkit.key_binding import KeyBindings
+from etm.view_textual import DynamicViewApp
+
+# from prompt_toolkit.key_binding import KeyBindings
 from etm.common import log_msg, display_messages
+
+from rich import print
+
+
+# def test():
+#     controller = Controller("example.db")
+#     db_manager = controller.db_manager
+#     last_instances = db_manager.get_last_instances()
+#     print("Last Instances:", last_instances[:5])
+#     next_instances = db_manager.get_next_instances()
+#     print("\nNext Instances:", next_instances[:5])
+#     next = controller.get_next()
+#     print("\nNext:")
+#     for item in next:
+#         print(item)
+#     last = controller.get_last()
+#     print("\nLast:")
+#     for item in last:
+#         print(item)
+#     search_results = controller.find_records(r"\ball day\b")
+#     for item in search_results:
+#         print(item)
 
 
 def main():
-    """
-    Entry point for setting up and running the FourWeekView application.
-    """
-    # Initialize the database manager
-    # db_manager = DatabaseManager("example.db")
-
-    # Initialize key bindings
-    bindings = KeyBindings()
-    log_msg(f"got bindings {bindings}")
-
     controller = Controller("example.db")
-    log_msg(f"got controller {controller}")
-
-    # Create an instance of FourWeekView with the database manager and bindings
-    view = FourWeekView(controller, bindings)
+    view = DynamicViewApp(controller)
     view.run()
 
 
 if __name__ == "__main__":
-    log_msg("Starting the application")
     main()
